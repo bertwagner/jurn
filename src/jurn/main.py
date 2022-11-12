80ecdd76ba810f368f76c787fd46c9ec09ad2e74
Bert Wagner <bertwagner@bertwagner.com>
2022-11-12 15:36:12 -0500

    outline date logic

--- src/jurn/main.py
+++ src/jurn/main.py
@@ -1,4 +1,5 @@
 import click
+from datetime import date
 
 
 # jurn log -m 'wrote some docs' -t work#jurn
@@ -12,7 +13,7 @@ import click
 @click.option("--early-end", "-ee", type=int, help="Stop jurn from executing if this amount of time has not passed since the last journal entry. Useful to supress jurn prompting too frequently if adding to a .bashrc file or schedule.")
 def cli(early_end):
     last_entry=5
-    if last_entry < early_end:
+    if early_end==None or last_entry < early_end:
         pass
     else:
         exit()
@@ -24,9 +25,30 @@ def log(message):
     click.echo(f"MESSAGE={message}")
 
 @cli.command()
-def print():
+@click.option("--duration", "-d", type=click.Choice(['day','week','month','year'], case_sensitive=False), help="Inclusive time period to print journal entries for.")
+@click.option('--date-start', "-ds", type=click.DateTime(formats=["%Y-%m-%d"]), help="Inclusive start date range.")
+@click.option('--date-end', "-de", type=click.DateTime(formats=["%Y-%m-%d"]), help="Inclusive end date range.")
+def print(duration,date_start,date_end):
     """Outputs journal entries to the terminal."""
-    click.echo(f"PRINTING")
+    
+    if date_start==None and date_end:
+        click.echo("Must specify --date_start if providing --date_end")
+    elif (duration and (date_start or date_end)):
+        click.echo("Must specify either --duration or --date_start and --date_end")
+    elif (duration==None and date_start==None and date_end==None):
+        duration="day"
+        click.echo(f'using default duration {duration}')
+    elif (duration):
+        click.echo(f'using duration {duration}')
+    elif (date_start):
+        click.echo(f'using date_start {date_start}')
+
+        if (date_end):
+            click.echo(f'using date_end {date_end}')
+        else:
+            date_end = date.today()
+            click.echo(f'using default date_end today {date_end}')
+
 
 if __name__ == '__main__':
     cli()
